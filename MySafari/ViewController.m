58035935ff05dec3a894c735a1542644d8b9dc12
irwingonzales92 <irwin@newdesto.com>
2015-01-07 13:56:10 -0800

    First Two Steps Done

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 
 @end
 
@@ -19,9 +21,5 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
